9785820db79c61ccd1dc29c070c7fe1c83a3a872
Bhatarasiri Slakkham <bhatarasiri.s@beryl8.com>
2021-05-21 10:43:50 +0700

    edit format

--- app.py
+++ app.py
@@ -8,27 +8,27 @@ from nltk.stem.snowball import SnowballStemmer
 import pickle
 
 tokenizer = RegexpTokenizer(r'[A-Za-z]+')
+snowball = SnowballStemmer("english")
+tfidVec = pickle.load(open('tfidVec_model.mod', 'rb'))
+loaded_model = pickle.load(open('svc_model.mod', 'rb'))
+
 def splitUrlIntoToken(url):
-    # print(url)
     return tokenizer.tokenize(url)
 
 def removeTextFromList(input, rmTxt):
     input = [x for x in input if (x not in rmTxt)]
     return input
 
-snowball = SnowballStemmer("english")
 def textNormByStemType(inputTxt, stemtype):
     inputTxt =[stemtype.stem(word) for word in inputTxt]
     return inputTxt
 
-tfidVec = pickle.load(open('tfidVec_model.mod', 'rb'))
-loaded_model = pickle.load(open('svc_model.mod', 'rb'))
-
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "Hello Flask-Heroku"
+def homePage():
+    return """<xmp>To check phishing site.
+Example use api by '/api?url=www.google.com'</xmp>"""
 
 @app.route('/api', methods=['GET'])
 def get_api():
@@ -40,12 +40,9 @@ def get_api():
     data = ' '.join(stemText)
     df = pd.DataFrame([data], columns = ['data'])
 
-    # print(df)
     vectorText = tfidVec.transform(df['data'])
-    # print(vectorText.shape)
 
     result = loaded_model.predict(vectorText)
-    # print(result)
     return result[0]
 
 if __name__ == "__main__":
